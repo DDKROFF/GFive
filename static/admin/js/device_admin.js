27e2cab4d070c4a185eee2d2cabbf8a7a392af9b
ВашÐВDDKROFF <ddkroff@gmail.com>
2026-03-10 17:36:58 +0300

    Фикс управления бдд

--- static/admin/js/device_admin.js
+++ static/admin/js/device_admin.js
@@ -2,18 +2,23 @@
     $(document).ready(function() {
         function toggleFields() {
             var type = $('#id_device_type').val();
+
+            // Сначала скрываем оба блока
+            $('.computer-fields').hide();
+            $('.console-fields').hide();
+
+            // Показываем нужный блок
             if (type === 'computer') {
                 $('.computer-fields').show();
-                $('.console-fields').hide();
             } else if (type === 'console') {
-                $('.computer-fields').hide();
                 $('.console-fields').show();
-            } else {
-                $('.computer-fields').hide();
-                $('.console-fields').hide();
             }
         }
+
+        // Вызываем функцию при загрузке
         toggleFields();
+
+        // И при изменении типа
         $('#id_device_type').change(toggleFields);
     });
 })(django.jQuery);
